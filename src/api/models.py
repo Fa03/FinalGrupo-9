64143706c2b7b97a6e259bba0e12c47d39b02557
luchemix <luchemix@gmail.com>
2021-04-20 00:40:03 +0000

    social media

--- src/api/models.py
+++ src/api/models.py
@@ -17,3 +17,20 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Productos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(120), unique=True, nullable=False)
+    precio = db.Column(db.Integer(), unique=False, nullable=False)
+    detalles = db.Column(db.String(250), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Productos %r>' % self.nombre
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
+            "precio": self.precio,
+            "detalles": self.detalles,
+        }
