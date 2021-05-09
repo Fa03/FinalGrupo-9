a97813e3e1869c0b07ee68a612728d09891bec19
Brandchal <Bchavarrial@hotmail.com>
2021-05-09 19:22:16 +0000

    css payStripe, other changes

--- src/front/js/pages/register2.js
+++ src/front/js/pages/register2.js
@@ -3,21 +3,18 @@ import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
 import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import { Link, useParams, Redirect } from "react-router-dom";
-import { Card, Form, Col } from "react-bootstrap";
 import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
-
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import { ModalTerms } from "../component/modalTerms";
 const useStyles = makeStyles(theme => ({
 	paper: {
 		marginTop: theme.spacing(8),
@@ -146,7 +143,7 @@ export default function SignUp() {
 								required
 								fullWidth
 								id="apellidos"
-								label="apellidos"
+								label="Apellidos"
 								onChange={handleInputChange}
 							/>
 						</Grid>
@@ -229,6 +226,9 @@ export default function SignUp() {
 								onChange={handleInputChange}
 							/>
 						</Grid>
+					</Grid>
+					<Grid item xs={12}>
+						<ModalTerms />
 					</Grid>
 					<Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
 						Registrarse
--- src/front/js/pages/retrivePass.js
+++ src/front/js/pages/retrivePass.js
@@ -66,7 +66,7 @@ export const RetrivePass = () => {
 						src="https://img.taste.com.au/lmVc1ciM/taste/2018/03/apr-18_apple-blueberry-pie-with-cinnamon-pastry-3000x2000-136232-1.jpg"
 						thumbnail
 					/>
-					<Card>
+					{/* 	<Card>
 						<Card.Body>
 							<Card.Title>Card Title</Card.Title>
 							<Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
@@ -77,7 +77,7 @@ export const RetrivePass = () => {
 							<Card.Link href="#">Card Link</Card.Link>
 							<Card.Link href="#">Another Link</Card.Link>
 						</Card.Body>
-					</Card>
+					</Card> */}
 				</Col>
 
 				<Col xs={4}>
